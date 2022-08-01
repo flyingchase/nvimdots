4b4fb12a0ad2a0becf650e5d16716b644d6cc7f0
wlzhou <zhongbingwang@yeah.net>
2022-08-01 20:55:05 +0800

    python env changes

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -109,7 +109,8 @@ local function load_options()
 			cache_enabled = 0,
 		}
 		vim.g.python_host_prog = "/usr/bin/python"
-		vim.g.python3_host_prog = "/usr/local/bin/python3"
+		-- vim.g.python3_host_prog = "/usr/local/bin/python3"
+		vim.g.python3_host_prog = "/Users/qlzhou/.pyenv/shims/python3"
 		-- vim.g.python_host_prog = "/Users/wang/.pyenv/versions/3.7.13/bin/python"
 		-- vim.g.python3_host_prog = "/Users/wang/.pyenv/versions/3.7.13/bin/python"
 	else
